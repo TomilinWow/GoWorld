36830b48f960015151b5b079a79390ad006847e5
tomilin <tomilin229@gmail.com>
2022-11-04 01:11:27 +0500

    change

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,15 +1,12 @@
-import '../styles/global.css'
-import '../styles/main.css'
+import '../styles/globals.css'
 import {Provider} from "react-redux";
 import {store} from "../store/store";
 
-// @ts-ignore
+
 function MyApp({ Component, pageProps }) {
-    return <Provider store={store}>
-        <Component {...pageProps} />
-    </Provider>
-
-
+  return <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
 }
 
 export default MyApp
