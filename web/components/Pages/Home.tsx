97eeb25ea635dbf963b8d02fe75a187323778a83
tomilin <tomilin229@gmail.com>
2022-11-04 14:55:42 +0500

    change

--- web/components/Pages/Home.tsx
+++ web/components/Pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <h1 className="text-2xl font-semibold">
-            Home page
+            Home page 1
         </h1>
     );
 };
